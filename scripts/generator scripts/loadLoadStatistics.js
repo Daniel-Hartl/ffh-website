fb6695a3bf5077461e459fc7cde9d95132ece9da
Daniel Hartl <115152881+Daniel-Hartl@users.noreply.github.com>
2025-06-06 00:54:29 +0200

    begun welcome banner

--- scripts/generator scripts/loadLoadStatistics.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    const tabs = document.getElementsByClassName('tabcontent');
-    
-    const statistics = await fetch('data/gallery.json').then(response => response.json());
-
-});

3d573607a257d77e1bbb93dedfe615f1946918b8
Daniel Hartl <115152881+Daniel-Hartl@users.noreply.github.com>
2025-07-23 03:20:35 +0200

    implemented slideshow

--- scripts/generator scripts/loadWelcome.js
+++ scripts/generator scripts/loadWelcome.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 function generateWelcome(div, statistics) {
 
     const h2 = document.createElement('p');
-    h2.innerText = `Wir sind eine Feuerwehr der Gemeinde Duggendorf mit derzeit ${statistics.Mitglieder} Mitgliedern und einer aktiven Mannschaft aus ${statistics.Aktive} Einsatzkräften. Im Jahr ${new Date().getFullYear()} rückten wir zu ${statistics.Einsätze} Einsätzen aus.`;
+    h2.innerText = `Wir sind eine Feuerwehr der Gemeinde Duggendorf mit derzeit ${statistics.Mitglieder} Mitgliedern und einer aktiven Mannschaft aus ${statistics.Aktive} Einsatzkräften. Im Jahr ${new Date().getFullYear()} rückten wir zu ${statistics.Einsätze} Einsätzen aus. ` +
+     "Willst du auch Teil unserer Wehr werden? Sprich uns bei unserer nächsten Veranstaltung an oder schreibe uns eine Nachricht!";
     div.appendChild(h2);
 }

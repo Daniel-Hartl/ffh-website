089d294fd34588bff35827251ec96da31963ed18
Daniel Hartl <115152881+Daniel-Hartl@users.noreply.github.com>
2025-10-02 20:34:00 +0200

    implemented marquee

--- marquee/marquee.js
+++ marquee/marquee.js
@@ -1,9 +1,8 @@
-root = document.getElementsByTagName('marquee')[0];
+const tab = "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0";
+
+root = document.getElementById('marquee').children[0];
+console.log(root);
 if (root) {
     const res = getAppointments()
-        .then(x => {
-            console.log(x);
-             root.innerHTML = x.map((y) => `${y.Zeit} <b>${y.Titel}</b> am ${y.Ort}`).join("\xa0\xa0. : .\xa0\xa0");
-             //root.innerText = x.map((y) => y.Ort).join("   . : .   ");
-    });
+        .then(x => root.innerHTML = "Anstehende Termine:" + tab + x.map((y) => `${y.Zeit} <b>${y.Titel}</b> am ${y.Ort}`).join(tab + "+++" + tab));
 }
